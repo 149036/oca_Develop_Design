37ace445f30ca6b4955f49c0a1e4bc67d92a7ee2
149036 <sqrt2c@gmail.com>
2023-12-02 13:27:23 +0900

    shingou

---
+++ shingou_action.py
@@ -0,0 +1,32 @@
+from enum import Enum
+import sys
+
+
+class Shingou(Enum):
+    RED = 1
+    BLUE = 2
+    YELLOW = 3
+
+
+args = sys.argv
+
+
+def act_shingou() -> Shingou:
+    args1 = int(args[1])
+
+    if args1 not in [data.value for data in Shingou]:
+        raise "信号機の色に対応していません"
+
+    if args1 is Shingou.RED.value:
+        print("とまれ")
+
+    if args1 is Shingou.BLUE.value:
+        print("すすめ")
+
+    if args1 is Shingou.YELLOW.value:
+        print("ちゅうい")
+
+    return Shingou(args1)
+
+
+print(act_shingou())
