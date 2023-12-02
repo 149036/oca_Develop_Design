85c91bbf5bb38d71707bc80bd87149470c443d25
149036 <sqrt2c@gmail.com>
2023-12-02 13:56:26 +0900

    closure

---
+++ try_closure.py
@@ -0,0 +1,27 @@
+def test_closure(check_value):
+    data = []
+
+    def add_data(checked_value):
+        if checked_value > check_value:
+            data.append(checked_value)
+
+    def get_data():
+        return data
+
+    return add_data, get_data
+
+
+if __name__ == "__main__":
+    add_data, get_data = test_closure(3)
+
+    add_data(2)
+    add_data(3)
+    add_data(4)
+    add_data(5)
+    add_data(15)
+    add_data(100)
+    add_data(-100)
+
+    data = get_data()
+
+    print(data)
